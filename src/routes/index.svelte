d8ceb277e41f313c3a5d2aaaabefbc4e10a2c526
kjmin <kjmin.kr@gmail.com>
2022-03-31 21:57:48 +0900

    print current time

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script>
+    let currentTime = new Date();
+    setInterval(() => {
+        currentTime = new Date();
+    }, 1000);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>The current time is: {currentTime}</p>
